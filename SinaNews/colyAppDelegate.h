1b18a042fe0850b215be06426b02be813f059f22
coly <User@colys-Mac.local>
2013-06-14 17:06:16 +0800

    Initial Commit

---
+++ SinaNews/colyAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  colyAppDelegate.h
+//  SinaNews
+//
+//  Created by coly on 13-6-14.
+//  Copyright (c) 2013年 coly. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface colyAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ SinaNews/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  SinaNews
+//
+//  Created by coly on 13-6-14.
+//  Copyright (c) 2013年 coly. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "colyAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+  @autoreleasepool {
+      return UIApplicationMain(argc, argv, nil, NSStringFromClass([colyAppDelegate class]));
+  }
+}
