6ed35bd3655697cd6ae6cfd4bd262bc9d74891cb
coly <zw1232002@163.com>
2013-07-23 23:10:04 +0800

    正在修改uiwebview样式

--- SinaNews/newsDetailWebView.m
+++ SinaNews/newsDetailWebView.m
@@ -23,9 +23,27 @@
 
 - (void)loadHTMLFromString:(NSString *)stringURL
 {
+  [self loadDocument:@"test.css"];
   NSURL *url = [NSURL URLWithString:stringURL];
   NSURLRequest *request = [NSURLRequest requestWithURL:url];
   [self loadRequest:request];
+}
+
+//加载本地使用说明文件文件
+-(void)loadDocument:(NSString *)docName
+{
+  NSString *mainBundleDirectory=[[NSBundle mainBundle] bundlePath];
+  NSString *path=[mainBundleDirectory stringByAppendingPathComponent:docName];
+  
+  NSURL *url=[NSURL fileURLWithPath:path];
+  NSURLRequest *request=[NSURLRequest requestWithURL:url];
+  [self loadRequest:request];
+}
+
+- (void)loadCss
+{
+  NSString *css =@"<style>body{padding:20px;background:#f00;}</style>";
+  [self loadHTMLString:css baseURL:nil];
 }
 
 /*
